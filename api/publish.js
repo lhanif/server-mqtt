77b4f8815d3e7bc4a70e36818e1f5645064a1a76
lhanif <lhanif467@gmail.com>
2025-09-24 19:39:04 +0700

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -11,15 +11,26 @@ export default async function handler(req, res) {
   }
 
   try {
-    const client = mqtt.connect('mqtt://broker.hivemq.com');
+    const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt', {
+      clientId: 'vercel-' + Math.random().toString(16).substr(2, 8),
+      clean: true,
+      reconnectPeriod: 0
+    });
+
     client.on('connect', () => {
-      client.publish(topic, message, () => {
+      console.log('Connected to HiveMQ');
+      client.publish(topic, message, { qos: 0 }, (err) => {
+        if (err) console.error('Publish error:', err);
         client.end();
       });
     });
+
+    client.on('error', (err) => console.error('MQTT error:', err));
+
+    // Kirim respon HTTP lebih dulu supaya GSM tidak timeout
     res.status(200).json({ status: 'published', topic, message });
   } catch (err) {
-    console.error(err);
+    console.error('Handler error:', err);
     res.status(500).json({ error: 'Publish failed' });
   }
 }
